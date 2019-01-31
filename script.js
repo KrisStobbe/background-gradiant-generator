ed0c0537edb5f4b0ef49bb3a1c03c02b849d3a03
Kris Stobbe <krisstobbe@yahoo.com>
2019-01-31 12:49:05 -0600

    Updating Gradient Support
    Whenever the Random Button is pressed, the two input buttons also reflect the change.
    
    This RGB-> Hex Conveter: https://stackoverflow.com/questions/14943074/html5-input-colors-default-color

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ var body = document.getElementById("gradient");
 //Adding Button Initialization
 var button = document.querySelector(".button");
 
+//Adding Buttons Value Initialization to Update them
+var current1 = document.getElementById("current1");
+var current2 = document.getElementById("current2");
+
 //Testing the Variables to the Log
 console.log(color1);
 console.log(color2);
@@ -28,7 +32,6 @@ function setGradient(){
 
 //Detecting when the Input of the Inputs Change
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
 
 //Adding Ability to Get Random Integers up to 255 (Will Need 6)
@@ -38,14 +41,20 @@ function getRandomInt(max) {
 
 //Adding function that changes Linear Gradient on Click
 function randomGradient(){
-	// var1 = getRandomInt(255);var2 = getRandomInt(255);
-	// var3 = getRandomInt(255);var4 = getRandomInt(255);
-	// var5 = getRandomInt(255);var6 = getRandomInt(255);
+	 var1 = getRandomInt(255);
+	 var2 = getRandomInt(255);
+	 var3 = getRandomInt(255);
+	 var4 = getRandomInt(255);
+	 var5 = getRandomInt(255);
+	 var6 = getRandomInt(255);
+
 	body.style.background = "linear-gradient(to right, rgb("
-	+ getRandomInt(255) +"," + getRandomInt(255) +"," + getRandomInt(255)
-	+ "), rgb(" + getRandomInt(255) +"," + getRandomInt(255) +"," + getRandomInt(255)
+	+ var1 +"," + var2 +"," + var3
+	+ "), rgb(" + var4 +"," + var5 +"," + var6
 	+ "))";
 	css.textContent = body.style.background + ";";
-
-	// document.getElementById("color1").setAttribute('value',="#3e22f7");
+	
+	//RGB to Hex Converter
+	current1.value="#" + ((1 << 24) + (var1 << 16) + (var2 << 8) + var3).toString(16).slice(1);
+	current2.value="#" + ((1 << 24) + (var4 << 16) + (var5 << 8) + var6).toString(16).slice(1);
 }
